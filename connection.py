02f41863a0ca83ade751862d4ee7e69bf315d3f0
JISHNU-SUNEESH <65233322+JISHNU-SUNEESH@users.noreply.github.com>
2024-04-29 15:18:28 +0530

    first commit

--- connection.py
+++ connection.py
@@ -2,15 +2,28 @@ import mysql.connector
 
 import logging
 
-logging.basicConfig(filename='connection.log',level=logging.INFO)
 
-logging.info("Creating connection")
+import os
+
+filepath='connection.log'
+
+if os.path.exists(filepath):
+    os.remove(filepath)
+    
+logger_conn=logging.getLogger('connection')
+logger_conn.setLevel(logging.INFO)
+
+con_handler=logging.FileHandler('connection.log')
+con_handler.setLevel(logging.INFO)
+logger_conn.addHandler(con_handler)
+
+logger_conn.info("Creating connection")
 connection=mysql.connector.connect(
-    user='jishnu',
-    password='jishnusuneesh',
+    user='root',
+    password='Js23242526@$',
     host='localhost',
     database='BI_ERP'
 )
 
 cursor=connection.cursor()
-logging.info("Cursor created")
+logger_conn.info("Cursor created")
--- purchase_order.py
+++ purchase_order.py
@@ -7,7 +7,13 @@ from connection import cursor
 filepath='purchase_order.log'
 if os.path.exists(filepath):
     os.remove(filepath)
-logging.basicConfig(filename='purchase_order.log',level=logging.INFO)
+
+logger_po=logging.getLogger('purchase_order')
+logger_po.setLevel(logging.INFO)
+po_handler=logging.FileHandler('purchase_order.log')
+po_handler.setLevel(logging.INFO)
+logger_po.addHandler(po_handler)
+
 
 app=Flask(__name__)
 
@@ -15,7 +21,7 @@ app=Flask(__name__)
 def po_creation():
     try:
         
-        logging.info("Fetching data from UI")
+        logger_po.info("Fetching data from UI")
         data=request.json
         po_number = data.get('po_number')
         date_issued = data.get('date_issued')
@@ -26,35 +32,94 @@ def po_creation():
         unit_price = data.get('unit_price')
         total_price = quantity * unit_price
 
-        logging.info(f"data collected successfully: {data}")
+        logger_po.info(f"data collected successfully: {data}")
 
-        logging.info("inserting data into table")
+        logger_po.info("inserting data into table")
 
 
    
     
 
-        query="INSERT INTO PurchaseOrder (PO_Number, DateIssued, VendorName, VendorContact, ItemDescription, Quantity, UnitPrice, TotalPrice) \
+        query="INSERT INTO BI_ERP.PurchaseOrders (PO_Number, DateIssued, VendorName, VendorContact, ItemDescription, Quantity, UnitPrice, TotalPrice) \
         VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
         val=(po_number, date_issued, vendor_name, vendor_contact, item_description, quantity, unit_price, total_price)
         
-        logging.info(f"Query:{query}")
+        logger_po.info(f"Query:{query}")
         
         cursor.execute(query,params=val)
 
-        logging.info("data loaded to purchaseorder")
+        logger_po.info("data loaded to purchaseorder")
         cursor.execute("commit;")
-        logging.info("closing the connection after commiting")
-        connection.commit
+        logger_po.info("closing the connection after commiting")
+        
 
         
         return jsonify({'message': 'Purchase order created successfully'}), 201
     
     except Exception as e:
-        logging.info(f"Some Error occured: {e}")
+        logger_po.info(f"Some Error occured: {e}")
         return ("Some error occured. Check logs")
     
 
+@app.route('/poupdate',methods=['put'])
+def po_update():
+    try:
+        logger_po.info("getting data for po update")
+        data=request.json
+        po_number=data.get('po_number')
+        date_issued = data.get('date_issued')
+        vendor_name = data.get('vendor_name')
+        vendor_contact = data.get('vendor_contact')
+        item_description = data.get('item_description')
+        quantity = data.get('quantity')
+        unit_price = data.get('unit_price')
+        total_price = quantity * unit_price
+        
+        logger_po.info(f"data collected successfully: {data}")
+
+        logger_po.info("Updating data into table")
+        
+        sql="update BI_ERP.PurchaseOrders set\
+            DateIssued=%s,\
+            VendorName=%s,\
+            VendorContact=%s,\
+            ItemDescription=%s,\
+            Quantity=%s,\
+            UnitPrice=%s,\
+            TotalPrice=%s\
+            where PO_Number=%s; "
+        val=( date_issued, vendor_name, vendor_contact, item_description, quantity, unit_price, total_price,po_number)
+        logger_po.info(f"Query being run: {sql}")
+        cursor.execute(sql,val)
+        logger_po.info("Updated the PO")
+        cursor.execute("commit;")
+        return jsonify({'message':'Po updated successfully'}),201
+        
+    except Exception as e:
+        logger_po.info(e)
+        return jsonify({'message':'some error occured !! Please check logs'})
+         
+
+
+@app.route('/podelete',methods=['delete'])
+def delete_po():
+    try:
+        logger_po.info("fetching po for deletion")
+        data=request.json
+        po_number=data.get('po_number')
+        
+        sql="delete from BI_ERP.PurchaseOrders where po_number in (%s);"
+        logger_po.info("deleting PO")
+        
+        cursor.execute(sql,params=(po_number,))
+        logger_po.info("Po deleted")
+        cursor.execute("commit;")
+        return jsonify({'message':'Po deleted successfully'}),201
+        
+    except Exception as e:
+        logger_po.info(e)
+        return jsonify({'message:':'some error occured while deletion!!'})
+    
 if __name__=='__main__':
     app.run(debug=True,threaded=True)
 
